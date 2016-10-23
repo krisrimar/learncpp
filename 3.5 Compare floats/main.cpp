d0dcd01f586740962070d37b83f6f6324bdd9f9f
krisrimar <rimarkris@gmail.com>
2016-10-23 13:03:50 +0300

    Initial commit
    fabs() from <cmath> library allows comparing absolute values of two
    numbers

---
+++ 3.5 Compare floats/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+  cout << fabs(5.2193192) << "\n";
+  cout << fabs(12.1231241) << "\n";
+  int some;
+  cin >> some;
+
+}
