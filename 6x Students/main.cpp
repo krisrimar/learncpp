82ac549a09c89d69d10efbfc50d306713ccc9612
krisrimar <rimarkris@gmail.com>
2016-10-23 15:55:12 +0300

    User input, for loop iterations, score compare refactor
    - Refactor number of iteration the secod for loop in sortScores() goes
    through (previously did not reach last array element) (line: 23)
    - Change biggest score comparison from simply comparing value to
    comparing value at a certain index (we later need to swap values at
    indexes) (line: 26)
    - Add do while loop to prevent user from entering a number of students
    smaller than 2 (line: 48)
    - Refactor assigning name and score value on user input (no need for
    additional variables) (line: 61)

--- 6x Students/main.cpp
+++ 6x Students/main.cpp
@@ -12,29 +12,28 @@ struct Student {
 
 void sortScores(vector<Student> &array, int *iterations) {
 
-  int *biggestNumber      = new int;
-  int *biggestNumberIndex = new int;
+  int *largestScoreIndex  = new int;
   bool *thereWereChanges  = new bool;
   *thereWereChanges       = false;
 
   for(int loop = 0; loop < *iterations; ++loop)
   {
-    cout << "loop #" << loop << '\n';
-    *biggestNumber = array[loop].score;
-    for(int index = loop + 1; index < *iterations - 1; ++index)
+    //cout << "loop #" << loop << '\n';
+    *largestScoreIndex = loop;
+    for(int index = loop + 1; index < *iterations; ++index)
     {
-      cout << "index #" << index << '\n';
-      if(array[index].score > *biggestNumber)
+      //cout << "index #" << index << '\n';
+      if(array[index].score > array[*largestScoreIndex].score)
       {
-        cout << "the new big number is " << array[index].score << '\n';
-        *biggestNumber = array[index].score;
-        *biggestNumberIndex = index;
+        //cout << "the new big number is " << array[index].score << '\n';
+        *largestScoreIndex = index;
         *thereWereChanges = true;
       }
     }
     if(*thereWereChanges)
     {
-      swap(array[loop], array[*biggestNumberIndex]);
+      //cout << "swapping " << array[*largestScoreIndex].score << "(" << *largestScoreIndex << ") with " << array[loop].score << "(" << loop << ")\n";
+      swap(array[loop], array[*largestScoreIndex]);
       *thereWereChanges = false;
     }
   }
@@ -43,34 +42,33 @@ void sortScores(vector<Student> &array, int *iterations) {
 int main()
 {
   vector<Student> studentsArr;
-
-  cout << "Please enter a number of students: ";
   int *numberOfStudents = new int;
-  cin >> *numberOfStudents;
 
+  //to prevent the user from entering: negative number, zero and 1
+  do {
+    cout << "Please enter a number of students: ";
+    cin >> *numberOfStudents;
+  } while (*numberOfStudents <= 1);
+
+  //change the size of the students array to fit in all of the students
   studentsArr.resize(*numberOfStudents);
   string *nameValue = new string;
-  int *scoreValue = new int;
+  int *scoreValue   = new int;
 
   //ask user to input all of the students names and scores
   for(int count = 0; count < *numberOfStudents; ++count)
   {
-    cout << "Enter name of student " << count+1 << ":   ";
-    cin >> *nameValue;
-    studentsArr[count].name = *nameValue;
-    cout << "Enter score of student " << count+1 << ":  ";
-    cin >> *scoreValue;
-    studentsArr[count].score = *scoreValue;
-    cout << "\n";
+    cout << "Enter name of student " << count + 1 << ":   ";
+    cin >> studentsArr[count].name;
+    cout << "Enter score of student " << count + 1 << ":  ";
+    cin >> studentsArr[count].score;
   }
 
   sortScores(studentsArr, numberOfStudents);
 
+  cout << '\n';
+
   for(const Student &element : studentsArr)
     cout << element.name << " " << element.score << '\n';
 
-  //for(int count = *numberOfStudents - 1; count > 0; --count)
-  //  cout << studentsArr[count].name << " " << studentsArr[count].score << '\n';
-
-  cin >> *nameValue;
 }
