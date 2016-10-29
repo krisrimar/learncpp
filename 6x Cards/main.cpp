2c1e6e81c58f083dab662d39923b9731d044f206
krisrimar <rimarkris@gmail.com>
2016-10-29 17:59:33 +0300

    Revert "Add getCardValue() for determining card point value"
    This reverts commit 56a5bfc0597d74f7d466cf2005c4fe25b5c5e5df.

--- 6x Cards/main.cpp
+++ 6x Cards/main.cpp
@@ -103,28 +103,6 @@ void shuffleDeck(array<Card, MAX_RANKS * MAX_SUITS> &deck)
   }
 }
 
-//returns the point value of a card depending on its rank
-int getCardValue(const Card &card)
-{
-  switch (card.rank) {
-    case RANK_2:      return 2;
-    case RANK_3:      return 3;
-    case RANK_4:      return 4;
-    case RANK_5:      return 5;
-    case RANK_6:      return 6;
-    case RANK_7:      return 7;
-    case RANK_8:      return 8;
-    case RANK_9:      return 9;
-    case RANK_10:
-    case RANK_JACK:
-    case RANK_QUEEN:
-    case RANK_KING:   return 10;
-    case RANK_ACE:    return 11;
-
-    default:          return 0;
-  }
-}
-
 int main()
 {
 
@@ -149,7 +127,5 @@ int main()
 
   shuffleDeck(cardDeck);
   printDeck(cardDeck);
-
-  cout << getCardValue(cardDeck[0]);
 
 }
