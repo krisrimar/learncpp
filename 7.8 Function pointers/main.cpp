70095fdc3cc828cf4ea75f4fa507e5bd8cdb9835
krisrimar <rimarkris@gmail.com>
2016-10-31 23:46:49 +0200

    Add add(), subtract(), multiply() and divide()
    Note: the divide() function has error handling for the user trying to
    divide by 0

--- 7.8 Function pointers/main.cpp
+++ 7.8 Function pointers/main.cpp
@@ -17,9 +17,36 @@ char getOperator()
   {
     cout << "Enter operator: ";
     cin >> mathOperator;
-  } while(mathOperator != '+' && mathOperator != '-' && mathOperator != '/' && mathOperator != '*');
+  }
+  while(mathOperator != '+' && mathOperator != '-' && mathOperator != '/' && mathOperator != '*');
 
   return mathOperator;
+}
+
+int add(int x, int y)
+{
+  return x + y;
+}
+
+int subtract(int x, int y)
+{
+  return x - y;
+}
+
+int multiply(int x, int y)
+{
+  return x * y;
+}
+
+int divide(int x, int y)
+{
+  if(y == 0)
+  {
+    cout << "Can't divide by zero :(";
+    return 0;
+  }
+  else
+    return x / y;
 }
 
 int main()
