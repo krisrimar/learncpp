9c4f6d8f37dac257daed70890eb88ba52ff40101
krisrimar <rimarkris@gmail.com>
2016-11-03 20:54:33 +0200

    Add Stack class
    Stack is a simple class that acts as the stack memory by pushing or
    popping values in it, or reseting overall

--- 8.2 Classes/main.cpp
+++ 8.2 Classes/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+
 class Employee
 {
 public:
@@ -38,6 +39,55 @@ class Point3d
 
 };
 
+class Stack
+{
+  int numbers[10];
+  int stackLen = 0;
+
+  public:
+    void reset()
+    {
+      for (int index = 0; index < stackLen; ++index)
+      {
+        numbers[index] = 0;
+      }
+      stackLen = 0;
+    }
+
+    bool push(int number)
+    {
+      if(stackLen < 10)
+        ++stackLen;
+      else
+        return false;
+      numbers[stackLen - 1] = number;
+      return true;
+    }
+
+    int pop()
+    {
+      if(stackLen < 1)
+      {
+        std::cout << "There's nothing to pop :(" << std::endl;
+        return 0;
+      }
+      --stackLen;
+      int returnValue = numbers[stackLen];
+      numbers[stackLen] = 0;
+      return returnValue;
+    }
+
+    void print()
+    {
+      std::cout << "( ";
+      for(int index = 0; index < stackLen; ++index)
+      {
+        std::cout << numbers[index] << " ";
+      }
+      std::cout << ")" << std::endl;
+    }
+};
+
 int main()
 {
     // Declare two employees
@@ -52,5 +102,23 @@ int main()
     point.setValues(1.0,2.0,3.0);
     point.print();
 
-    return 0;
+    Stack stack;
+  	stack.reset();
+
+  	stack.print();
+
+  	stack.push(5);
+  	stack.push(3);
+  	stack.push(8);
+  	stack.print();
+
+  	stack.pop();
+  	stack.print();
+
+  	stack.pop();
+  	stack.pop();
+
+  	stack.print();
+
+  	return 0;
 }
