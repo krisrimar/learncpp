68b12c03a8644e04adf29df75be73c6f22cf942f
krisrimar <rimarkris@gmail.com>
2016-11-06 01:09:16 +0200

    Add distanceTo()
    - Add distanceTo() which determines the distance between two points
    - Add <cmath> lib for sqrt() function

--- 8x Point2d/main.cpp
+++ 8x Point2d/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 class Point2d
 {
@@ -13,14 +14,19 @@ class Point2d
     {
       std::cout << "Point2d(" << m_x << ", " << m_y << ")\n";
     }
+    double distanceTo(Point2d &p)
+    {
+      return sqrt((m_x - p.m_x)*(m_x - p.m_x) + (m_y - p.m_y)*(m_y - p.m_y));
+    }
 };
 
 int main()
 {
-    Point2d first;
-    Point2d second(3.0, 4.0);
-    first.print();
-    second.print();
+  Point2d first;
+  Point2d second(3.0, 4.0);
+  first.print();
+  second.print();
+  std::cout << "Distance between two points: " << first.distanceTo(second) << '\n';
 
-    return 0;
+  return 0;
 }
