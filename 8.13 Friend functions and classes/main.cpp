ab226fc9b70288854ebdbb14d85f01de674f1b73
krisrimar <rimarkris@gmail.com>
2016-11-05 22:46:59 +0200

    Complete overhaul of the previous code
    Note: previously I have been uploading code only when it was quiz time,
    but lately the lessons themselves are worth uploading to keep in mind

--- 8.13 Friend functions and classes/main.cpp
+++ 8.13 Friend functions and classes/main.cpp
@@ -1,48 +1,50 @@
 #include <iostream>
 
-class Display;
-
-class Storage
+class Vector3d
 {
   private:
-    int m_nValue;
-    double m_dValue;
+    double m_x, m_y, m_z;
 
   public:
-    Storage(int nValue, double dValue)
+    Vector3d(double x = 0.0, double y = 0.0, double z = 0.0) : m_x{x}, m_y{y}, m_z{z} {}
+
+    void print()
     {
-      m_nValue = nValue;
-      m_dValue = dValue;
+      std::cout << "Vector(" << m_x << ", " << m_y << ", " << m_z << ")\n";
     }
 
-    friend class Display;
+    friend class Point3d;
 };
 
-class Display
+class Point3d
 {
-  private:
-    bool m_displayIntFirst;
+private:
+  double m_x, m_y, m_z;
 
-  public:
-    Display(bool displayIntFirst) { m_displayIntFirst = displayIntFirst; }
+public:
+  Point3d(double x = 0.0, double y = 0.0, double z = 0.0) : m_x{x}, m_y{y}, m_z{z} {}
 
-    //depending on what will be passed as an argument to this function
-    //it will get member values of Storage class and display them in a sequence
-    void displayItem(Storage &storage)
-    {
-      if(m_displayIntFirst)
-        std::cout << storage.m_nValue << " " << storage.m_dValue << '\n';
-      else //display double first
-        std::cout << storage.m_dValue << " " << storage.m_nValue << '\n';
-    }
-}
+  void print()
+  {
+    std::cout << "Point(" << m_x << ", " << m_y << ", " << m_z << ")\n";
+  }
+
+  void moveByVector(Vector3d &v)
+  {
+    m_x += v.m_x;
+    m_y += v.m_y;
+    m_z += v.m_z;
+  }
+};
 
 int main()
 {
-  Storage storage(5, 6.7);
-  Display display(false);
+  Point3d p(1.0, 2.0, 3.0);
+  Vector3d v(2.0, 2.0, -3.0);
 
-  display.displayItem(storage);
+  p.print();
+  p.moveByVector(v);
+  p.print();
 
   return 0;
 }
