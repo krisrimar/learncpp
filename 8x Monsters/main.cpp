b44f3c898750468c745606fbd4270411a14dcdf0
krisrimar <rimarkris@gmail.com>
2016-11-06 10:39:28 +0200

    Move MonterType enum to Monster class
    Note: MonsterType is specific only to the Monster class, so no need to
    have it outside of the class

--- 8x Monsters/main.cpp
+++ 8x Monsters/main.cpp
@@ -1,18 +1,5 @@
 #include <iostream>
 
-enum MonsterType
-{
-  MONSTER_DRAGON,
-  MONSTER_GOBLIN,
-  MONSTER_OGRE,
-  MONSTER_ORC,
-  MONSTER_SKELETON,
-  MONSTER_TROLL,
-  MONSTER_VAMPIRE,
-  MONSTER_ZOMBIE,
-  MAX_MONSTER_TYPES
-};
-
 class Monster
 {
   private:
@@ -20,6 +7,20 @@ class Monster
     std::string m_name;
     std::string m_roar;
     int m_hitPoints;
+
+  public:
+    enum MonsterType
+    {
+      MONSTER_DRAGON,
+      MONSTER_GOBLIN,
+      MONSTER_OGRE,
+      MONSTER_ORC,
+      MONSTER_SKELETON,
+      MONSTER_TROLL,
+      MONSTER_VAMPIRE,
+      MONSTER_ZOMBIE,
+      MAX_MONSTER_TYPES
+    };
 };
 
 int main()
