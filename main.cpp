6fb892f6e770dac13ab42e02486c711578756d27
krisrimar <rimarkris@gmail.com>
2016-11-08 23:38:14 +0200

    Add >> operator overload to Fraction class
    - Add default values to parameters of default Fraction class
    constructor to initialize new objects that have no arguments

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ class Fraction
     int mDenominator = 1;
 
   public:
-    Fraction(int numerator, int denominator) : mNumerator{numerator}, mDenominator{denominator}
+    Fraction(int numerator = 0, int denominator = 1) : mNumerator{numerator}, mDenominator{denominator}
     {
       reduce();
     }
@@ -33,8 +33,10 @@ class Fraction
     friend Fraction operator*(const Fraction &f1, const Fraction &f2);
     friend Fraction operator*(const Fraction &f1, const int &f2);
     friend Fraction operator*(const int &f1, const Fraction &f2);
-    //output friend fuction
     friend std::ostream& operator<<(std::ostream &out, const Fraction &f);
+    friend std::ostream& operator<<(std::ostream& out, const Fraction &f);
+    friend std::istream& operator>>(std::istream& in, Fraction &f);
+
 };
 
 Fraction operator*(const Fraction &f1, const Fraction &f2)
@@ -58,39 +60,51 @@ Fraction operator*(const int &f1, const Fraction &f2)
   return f3;
 }
 
-std::ostream& operator<< (std::ostream &out, const Fraction &f)
+std::ostream& operator<< (std::ostream& out, const Fraction &f)
 {
   out << f.mNumerator << "/" << f.mDenominator;
-
   return out;
+}
+
+std::istream& operator>> (std::istream& in, Fraction &f)
+{
+  in >> f.mNumerator;
+  in >> f.mDenominator;
+
+  return in;
 }
 
 int main()
 {
   Fraction f1(2, 5);
-    f1.print();
+  f1.print();
 
-    Fraction f2(3, 8);
-    f2.print();
+  Fraction f2(3, 8);
+  f2.print();
 
-    Fraction f3 = f1 * f2;
-    f3.print();
+  Fraction f3 = f1 * f2;
+  f3.print();
 
-    Fraction f4 = f1 * 2;
-    f4.print();
+  Fraction f4 = f1 * 2;
+  f4.print();
 
-    Fraction f5 = 2 * f2;
-    f5.print();
+  Fraction f5 = 2 * f2;
+  f5.print();
 
-    Fraction f6 = Fraction(1, 2) * Fraction(2, 3) * Fraction(3, 4);
-    f6.print();
+  Fraction f6 = Fraction(1, 2) * Fraction(2, 3) * Fraction(3, 4);
+  f6.print();
 
-    std::cout << f6;
+  std::cout << f6;
 
-    Cents cents1(6);
-    Cents cents2(8);
+  Cents cents1(6);
+  Cents cents2(8);
 
-    Cents centsSum = cents1 + cents2; // without the prototype in Cents.h, this would fail to compile
+  Cents centsSum = cents1 + cents2; // without the prototype in Cents.h, this would fail to compile
 
-    return 0;
+  std::cout << "Enter fraction: ";
+  Fraction f;
+  std::cin >> f;
+  std::cout << f;
+
+  return 0;
 }
