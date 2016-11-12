2fb72a18f99a6cd15cbf80dea43557dcd037e6ed
krisrimar <rimarkris@gmail.com>
2016-11-12 10:28:35 +0200

    Add FixedPoint2(double) constructor
    - Add FixedPoint2(double) constructor which converts 9.617 into mBase =
    9, mDecimal = 62 (from .617)

--- main.cpp
+++ main.cpp
@@ -10,10 +10,25 @@ class FixedPoint2
 
   public:
     FixedPoint2(std::int16_t base = 0, std::int8_t decimal = 0) : mBase{base}, mDecimal{decimal} {}
+
+    FixedPoint2(double d)
+    {
+      //get the non-fractional (mBase) component
+      //this is done by casting double into an integer
+      mBase = tatic_cast<std::int16_t>(d); //truncates fraction
+
+      //now we get the fractional component
+      //d - mBase leaves only the fractional portion e.g. 9.617 minus 9.0 = 0.617
+      //multiplied by 100 moves the point 2 positions to the left e.g. 0.617 -> 61.7
+      //then we round it to the nearest integral e.g. 61.7 -> 62.0
+      //and finally cast to int8_t to drop the decimals e.g. 62.0 -> 62
+      mDecimal = static_cast<std::int8_t>(round((d - mBase) * 100));
+    }
 };
 
 int main()
 {
   FixedPoint2 a(10,55);
+  FixedPoint2 b(9.617);
   return 0;
 }
