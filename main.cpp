6d55a27edb075671d6f5a4e29e09d806bc38207a
krisrimar <rimarkris@gmail.com>
2016-11-12 19:37:59 +0200

    Update Creature class
    - Make all get functions const
    - Refactor isDead() function

--- main.cpp
+++ main.cpp
@@ -18,36 +18,26 @@ class Creature
     }
 
     //GET
-    std::string getName() { return mName; }
-    char getSymbol() { return mSymbol; }
-    int getHealthPoints() { return mHealthPoints; }
-    int getDamagePoints() { return mDamagePoints; }
-    int getGold() { return mGold; }
+    const std::string getName() const { return mName; }
+    const char getSymbol() const      { return mSymbol; }
+    const int getHealthPoints() const { return mHealthPoints; }
+    const int getDamagePoints() const { return mDamagePoints; }
+    const int getGold() const         { return mGold; }
 
     //check if the creature is dead
-    bool isDead()
-    {
-      if(mHealthPoints <= 0)
-        return true;
-      return false;
-    }
+    bool isDead() const { return mHealthPoints <= 0; }
 
     //SET
-    void reduceHealth(int damage)
-    {
-      mHealthPoints -= damage;
-    }
-
-    void addGold(int amount)
-    {
-      mGold += amount;
-    }
+    void reduceHealth(int damage) { mHealthPoints -= damage; }
+    void addGold(int amount) { mGold += amount; }
 
 };
 
 int main()
 {
   Creature creature("Orc", 'o', 200, 10, 30);
+  creature.addGold(10);
+  creature.reduceHealth(123);
 
   std::cout << "The creatures name is " << creature.getName() << " its symbol is " << creature.getSymbol() << " it has " << creature.getHealthPoints() << "HP, the damage it makes is " << creature.getDamagePoints() << "DPH and the amount of gold it carries is " << creature.getGold() << "G\n";
 
