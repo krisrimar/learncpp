d2f2896b437c696afc4c8a0b089900ac79c5c59b
krisrimar <rimarkris@gmail.com>
2016-11-12 10:19:30 +0200

    Add default constructor (FixedPoint2 class)

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 
 class FixedPoint2
 {
+  private:
+    std::int16_t mBase; //non-fractional part ->1.x
+    std::int8_t mDecimal; //fractional part x.0<-
 
+  public:
+    FixedPoint2(std::int16_t base = 0, std::int8_t decimal = 0) : mBase{base}, mDecimal{decimal} {}
 };
 
 int main()
 {
+  FixedPoint2 a(10,55);
   return 0;
 }
