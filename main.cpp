c09ebb5b9f77d7d8754f2e0d16af98b9ae5045aa
krisrimar <rimarkris@gmail.com>
2016-11-20 10:21:07 +0200

    Add Average template function
    Note: to create a template function you need to declare the template
    right above it

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 
 template <typename anyNumberType>
-
 anyNumberType add(anyNumberType x, anyNumberType y)
 {
   return x + y;
+}
+
+template <class anyArrayType>
+anyArrayType Average(anyArrayType *atArray, int nNumValues)
+{
+  anyArrayType tSum = 0;
+  for(int nCount = 0; nCount < nNumValues; nCount++)
+    tSum += atArray[nCount];
+
+  tSum /= nNumValues;
+  return tSum;
 }
 
 
@@ -12,6 +22,12 @@ int main()
 {
   std::cout << add(12, 3) << std::endl;
   std::cout << add(5.2,5.5) << std::endl;
+
+  int iSomeArray[] = {1,2,3,4,5};
+  double dSomeArray[] = {23.4,12.3,78.1,90.3,9.0};
+
+  std::cout << Average(iSomeArray, 5) << std::endl;
+  std::cout << Average(dSomeArray, 5) << std::endl;
 
   return 0;
 }
